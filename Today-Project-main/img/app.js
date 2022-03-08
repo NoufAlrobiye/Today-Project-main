4d6b6dcc02763ba0d9930616a4fc6426a75c4970
NoufAlrobiye <Nouf.alrobiye@gmail.com>
2022-03-08 19:10:20 +0300

    edit the weather

--- Today-Project-main/img/app.js
+++ Today-Project-main/img/app.js
@@ -39,49 +39,42 @@ if (navigator.geolocation){
 }
 })  */
 
-
+console.log("test")
 let weather = {
     apiKey: "27ddadc485554c5bb84e95511a56aa0a",
     fetchWeather: function (city) {
       fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        "https://api.weatherbit.io/v2.0/current?city=" +
           city +
-          "&units=metric&appid=" +
+          "&units=metric&key=" +
           this.apiKey
       )
-        .then((response) => {
-          if (!response.ok) {
-            alert("No weather found.");
-            throw new Error("No weather found.");
-          }
-          return response.json();
-        })
-        .then((data) => this.displayWeather(data));
+        .then((response) => response.json()) 
+        .then((data) => this.displayWeather(data.data[0]));
     },
     displayWeather: function (data) {
-      const { name } = data;
-      const { icon, description } = data.weather[0];
-      const { temp, humidity } = data.main;
-      const { speed } = data.wind;
+      console.log(data)
+       const { name } = data;
+       const { icon, description } = data.weather;
+       const { temp } = data.temp;
+      const { speed } = data.wind_spd;
       document.querySelector(".city").innerText = "Weather in " + name;
-      document.querySelector(".icon").src =
-        "https://openweathermap.org/img/wn/" + icon + ".png";
-      document.querySelector(".description").innerText = description;
-      document.querySelector(".temp").innerText = temp + "°C";
-      document.querySelector(".humidity").innerText =
-        "Humidity: " + humidity + "%";
-      document.querySelector(".wind").innerText =
-        "Wind speed: " + speed + " km/h";
-      document.querySelector(".weather").classList.remove("loading");
-      document.body.style.backgroundImage =
-        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+       document.querySelector(".icon").src =
+       "https://www.weatherbit.io/static/img/icons/" + icon + ".png";
+       document.querySelector(".description").innerText = description;
+       document.querySelector(".temp").innerText = temp + "°C";
+       document.querySelector(".wind").innerText ="Wind speed: " + speed + " km/h";
+       document.querySelector(".weather").classList.remove("loading");
+       document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + name + "')";
     },
+
     search: function () {
       this.fetchWeather(document.querySelector(".search-bar").value);
     },
   };
   
   document.querySelector(".search button").addEventListener("click", function () {
+    console.log("test")
     weather.search();
   });
   
